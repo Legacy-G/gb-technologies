3cac3d2e3bac5c32dd729f7d522c104233de06b1
Gbure G Thomas <gburethomass@gmail.com>
2026-01-29 14:06:20 +0100

    deploy4

--- webint/settings.py
+++ webint/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +122,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'web' / 'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
