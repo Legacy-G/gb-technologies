117550a60c9febd09fca6caaf9e9d4e1c43f51f4
Gbure G Thomas <gburethomass@gmail.com>
2026-01-30 16:25:40 +0100

    another123

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.12 get-pip.py
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
